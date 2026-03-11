9335ca07d242c5f8b7f689a658b159d5bf3a9b9f
shqawthmw060-sketch <shqawthmw060@gmail.com>
2026-03-12 00:15:28 +0200

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,3 @@
-// بيانات سلايدر تجريبية، يمكن ربطها بTMDb API
-const slidesData = [
-  {title:"Avengers: Endgame", img:"https://image.tmdb.org/t/p/w1280/or06FN3Dka5tukK1e9sl16pB3iy.jpg"},
-  {title:"Joker", img:"https://image.tmdb.org/t/p/w1280/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg"},
-  {title:"Inception", img:"https://image.tmdb.org/t/p/w1280/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg"},
-  {title:"The Dark Knight", img:"https://image.tmdb.org/t/p/w1280/qJ2tW6WMUDux911r6m7haRef0WH.jpg"},
-];
-
-const heroSlider = document.getElementById("hero-slider");
-
 slidesData.forEach((slide,index)=>{
   const div = document.createElement("div");
   div.className = "slide";
@@ -29,3 +19,29 @@ setInterval(()=>{
 },5000);
 
 // CSS تأثيرات إضافية: slide-in, scale, opacity
+const slidesData = [
+  {title:"The Dark Knight", img:"images/darkknight.png"},
+  {title:"Joker", img:"images/joker.png"},
+  {title:"Avengers Endgame", img:"images/avengers.png"},
+];
+
+const heroSlider = document.getElementById("hero-slider");
+
+slidesData.forEach((slide,index)=>{
+  const div=document.createElement("div");
+  div.className="slide";
+  if(index===0) div.classList.add("active");
+  div.innerHTML=`
+    <img src="${slide.img}" class="slide-img">
+    <div class="caption"><h1>${slide.title}</h1></div>
+  `;
+  heroSlider.appendChild(div);
+});
+
+let current=0;
+setInterval(()=>{
+  const slides=document.querySelectorAll(".slide");
+  slides.forEach(s=>s.classList.remove("active"));
+  slides[current].classList.add("active");
+  current=(current+1)%slides.length;
+},5000);
